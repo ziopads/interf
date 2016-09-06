cac030600762b8db067297750f024c770b393d22
J. Haskins <ziopads@gmail.com>
2016-09-05 23:54:25 -0600

    update sketch.js with socket code

--- public/sketch.js
+++ public/sketch.js
@@ -22,17 +22,46 @@ function receiveOsc(address, value) {
 	}
 }
 
+// function sendOsc(address, value) {
+// 	socket.emit('message', [address].concat(value));
+// }
+
+// function setupOsc(oscPortIn, oscPortOut) {
+// 	var socket = io.connect('http://127.0.0.1', { port: 8081, rememberTransport: false });
+// 	socket.on('connect', function() {
+// 		socket.emit('config', {	
+// 			server: { port: oscPortIn,  host: '127.0.0.1'},
+// 			client: { port: oscPortOut, host: '127.0.0.1'}
+// 		});
+// 	});
+// 	socket.on('message', function(msg) {
+// 		if (msg[0] == '#bundle') {
+// 			for (var i=2; i<msg.length; i++) {
+// 				receiveOsc(msg[i][0], msg[i].splice(1));
+// 			}
+// 		} else {
+// 			receiveOsc(msg[0], msg.splice(1));
+// 		}
+// 	});
+// }
+
+
+
+
 function sendOsc(address, value) {
-	socket.emit('message', [address].concat(value));
+	socket.emit('message', [address, value]);
 }
 
 function setupOsc(oscPortIn, oscPortOut) {
-	var socket = io.connect('http://127.0.0.1', { port: 8081, rememberTransport: false });
+	socket = io.connect('http://127.0.0.1', { port: 8081, rememberTransport: false });
 	socket.on('connect', function() {
 		socket.emit('config', {	
 			server: { port: oscPortIn,  host: '127.0.0.1'},
 			client: { port: oscPortOut, host: '127.0.0.1'}
 		});
+	});
+	socket.on('connect', function() {
+		isConnected = true;
 	});
 	socket.on('message', function(msg) {
 		if (msg[0] == '#bundle') {
